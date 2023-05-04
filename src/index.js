45b7391c803e7ca8fe8f6bcaaff27846c47d7f9c
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-04 11:16:28 -0300

    Configurando projeto para começar a inicialização do Cost. Fonte Open Sans ja foi colocada,Favicon e images que vamos usar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './Componentes/index.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +12,3 @@ root.render(
 );
 
 
-reportWebVitals();
