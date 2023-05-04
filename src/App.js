04381a38f5663c5398e0c4eb9b7cb88b86ab529c
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-04 11:52:07 -0300

    Criando um react Router dom, primeiros passos da barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
-
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import Home from './Componentes/pages/Home';
+import NewProject from './Componentes/pages/NewProject';
+import Company from './Componentes/pages/Company';
+import Contact from './Componentes/pages/Contact';
 function App() {
   return (
     <div className="App">
-      <p>Cost</p>
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/company">Compania</Link></li>
+          <li><Link to="/contact">Contato</Link></li>
+          <li><Link to="/newproject">Novo Projeto</Link></li>
+        </ul>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
